3cc131a46eae66d761aa1e5a890fb17a15b80968
Gorka Eraña <eranagorka@gmail.com>
2024-11-12 00:46:16 +0100

    Better documented

--- scripts/scrape_components.py
+++ scripts/scrape_components.py
@@ -1,3 +1,10 @@
+"""
+Scrape metadata used for MDL validation from
+https://developer.veevavault.com/mdl/components/.
+
+Usage: `python3 scrape_components.py`
+"""
+
 from __future__ import annotations
 import json
 import re
@@ -12,13 +19,17 @@ import httpx
 Record: TypeAlias = dict[str, Any]
 
 
+URL = "https://developer.veevavault.com/mdl/components/"
+COMPONENT_NAME_PATTERN = re.compile(r"^[A-Z][a-z]+$")
+
+
 def table_to_records(tag: Tag) -> list[Record]:
     assert tag.name == "table"
     column_names = [
         t.text for t in tag.findChild("thead").findChild("tr").findChildren("th")
     ]
     records: list[Record] = []
-    # rows
+    # Rows
     for tr_tag in tag.findChild("tbody").findChildren("tr"):
         record: Record = {}
         # Columns
@@ -48,20 +59,24 @@ def find_in_siblings_until(
         next_tag = next_tag.find_next_sibling()
 
 
-URL = "https://developer.veevavault.com/mdl/components/"
-response = httpx.get(URL)
-soup = BeautifulSoup(response.content, "html.parser")
+class ComponentMetadata(msgspec.Struct):
+    """A struct holding component metadata scrapped from
+    https://developer.veevavault.com/mdl/components/.
 
-COMPONENT_NAME_PATTERN = re.compile(r"^[A-Z][a-z]+$")
+    `name`: component name.
+    `table`: is the parsed table with the attribute metadata.
+    `children`: are the children components in the subsections of the parent
+    component.
+    """
 
-
-class Component(msgspec.Struct):
     name: str
     table: list[Record]
-    children: list[Component] = []
+    children: list[ComponentMetadata] = []
 
     def convert(self):
-        # TODO: parse type checking, etc.
+        """Massage the component metadata into a more dumpable and easily
+        understandable format
+        """
         return (
             self.name,
             {
@@ -79,10 +94,33 @@ class Component(msgspec.Struct):
         )
 
 
-components = []
-for tag in soup.find_all("h1"):
-    if COMPONENT_NAME_PATTERN.match(tag.text):
+def main():
+    response = httpx.get(URL)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    components = []
+    # In summary:
+    # - component names are contained in `h1` headers;
+    # - the metadata of the attributes associated with a given component are
+    # within a table under the `h1` header;
+    # - subcomponents of the main component are contained `h3` headers, under
+    # which one can also find tables with the metadata of the attributes
+    # allowed under the subcomponent.
+
+    # Algorithmically, we proceed as follows:
+    # 1. Iterate over all `h1` headers the text of which start with one capital
+    # letter.
+    # 2. Find the first table after the `h1` header
+    # 3. Iterate over all `h3` headers before the next `h1` header
+    # 4. Gather the tables following the an `h3` header and before the next `h3`
+    # header
+    for tag in soup.find_all("h1"):
+        # Step 1
+        if not COMPONENT_NAME_PATTERN.match(tag.text):
+            continue
+        # Start gathering data to be put in a `ComponentMetadata` struct
         component = {"name": tag.text}
+        # Step 2
         table_tag = next(
             find_in_siblings_until(
                 tag, lambda t: t.name == "table", lambda t: t.name == "h3"
@@ -90,23 +128,32 @@ for tag in soup.find_all("h1"):
         )
         component["table"] = table_to_records(table_tag)
         component["children"] = []
+        # Step 3
         for heading3_tag in find_in_siblings_until(
             tag, lambda t: t.name == "h3", lambda t: (t is None) or (t.name == "h1")
         ):
             subcomponent = {"name": heading3_tag.text}
+            # Step 4
             subtable_tag = next(
                 find_in_siblings_until(
-                    heading3_tag, lambda t: t.name == "table", lambda t: t.name == "h3"
+                    heading3_tag,
+                    lambda t: t.name == "table",
+                    lambda t: t.name == "h3",
                 )
             )
             subcomponent["table"] = table_to_records(subtable_tag)
             component["children"].append(subcomponent)
         components.append(component)
 
-# Validation
-validated = msgspec.convert(components, type=list[Component])
+    # Poor man's validation
+    validated = msgspec.convert(components, type=list[ComponentMetadata])
 
-dumpable = {k: v for k, v in [c.convert() for c in validated]}
-(Path(__file__).parent.parent / "src" / "mdl" / "validation.json").write_text(
-    json.dumps(dumpable, indent=4)
-)
+    # Aaaaaand out it goes
+    dumpable = {k: v for k, v in [c.convert() for c in validated]}
+    out_path = Path(__file__).parent.parent / "src" / "mdl" / "validation.json"
+    out_path.write_text(json.dumps(dumpable, indent=4))
+    print(f"Wrote {out_path}")
+
+
+if __name__ == "__main__":
+    main()
