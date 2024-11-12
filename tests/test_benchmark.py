1255b91d10f3fbc530ce7b21518d66ac9aa0f084
Gorka Eraña <eranagorka@gmail.com>
2024-11-12 02:01:34 +0100

    Slight refactor of benchmark tests

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,3 +1,4 @@
+from operator import attrgetter
 from pathlib import Path
 
 import pytest
@@ -5,10 +6,26 @@ import pytest
 from mdl import Command
 
 
-@pytest.mark.parametrize(
-    "path",
-    sorted(Path(__file__).parent.rglob("*.mdl"), key=lambda p: p.name)
+path_name = attrgetter("name")
+# "ADD" and "MODIFY" commands are only allowed as subcommands under
+# "ALTER" MDL command
+mdl_files = sorted(
+    (
+        p
+        for p in Path(__file__).parent.rglob("*.mdl")
+        if p.name not in {"add.mdl", "modify.mdl"}
+    ),
+    key=path_name,
 )
-def test_something(path, benchmark):
+
+
+@pytest.mark.parametrize("path", mdl_files, ids=path_name)
+def test_loading(path, benchmark):
+    benchmark(Command.loads, path.read_text())
+    assert True
+
+
+@pytest.mark.parametrize("path", mdl_files, ids=path_name)
+def test_validating(path, benchmark):
     command = benchmark(Command.loads, path.read_text())
     assert command.validate()
