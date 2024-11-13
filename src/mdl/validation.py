8b3f0231193a55c93cac2683fd5c6a22cdee9746
Gorka Eraña <gera@baselifescience.com>
2024-11-13 12:55:29 +0100

    Correct type generation, only four failing tests to go

--- src/mdl/validation.py
+++ src/mdl/validation.py
@@ -29,27 +29,36 @@ def comment_to_type(comment: str) -> Type:
     allowed_vals_match = re.search(r"Allowed values : (.*)", comment, re.MULTILINE)
     allow_multiple_vals_match = re.search(r"Allows multiple values", comment)
     type_ = supported_types.get(matched_type_name)
+    # Notice two things in what follows:
+    # 1. All attribute values seem to be nullable, hence the `None` at every return.
+    # PLenty of examples under `tests/mdl_examples/scrapped`.
+    # 2. If an attribute value "allows multiple values", and it contains one item;
+    # the parser will parse it as a single value, hence the `list[a_type] | a_type`
+    # pattern below.
+
     # `matched_type_name` might not map to any Python built-in type, but it might
-    # be a component type
+    # be a component type name
     if type_ is None and (
         # Is it a component type?
         (matched_type_name in component_type_metadata.keys())
-        # If it a subcomponent type?
-        or any(matched_type_name in v.keys() for v in component_type_metadata.values())
+        # Is it a subcomponent type?
+        or any(
+            matched_type_name in v["subcomponents"].keys()
+            for v in component_type_metadata.values()
+        )
     ):
+        # TODO: this current validation implementation does not allow to validate a list
+        # of references, as Annotated[list[str], msgspec.Meta(pattern=rf"^{matched_type_name}\.")]
+        # is not a valid msgspec type constraing
         return Annotated[str, msgspec.Meta(pattern=rf"^{matched_type_name}\.")] | None
     if (allow_multiple_vals_match is not None) and (allowed_vals_match is not None):
-        return (
-            list[Literal[*(s for s in allowed_vals_match.groups(0)[0].split("|") if s)]]
-            | None
-        )
+        values = [s for s in allowed_vals_match.groups(0)[0].split("|") if s]
+        return list[Literal[*values]] | Literal[*values] | None
     elif allow_multiple_vals_match is not None:
-        return list[type_] | None
+        return list[type_] | type_ | None
     elif allowed_vals_match is not None:
-        return (
-            Literal[*(s for s in allowed_vals_match.groups(0)[0].split("|") if s)]
-            | None
-        )
+        allowed_values = [s for s in allowed_vals_match.groups(0)[0].split("|") if s]
+        return Literal[*allowed_values] | None
     if any(m is not None for m in [max_len_match, min_val_match, max_val_match]) and (
         type_ is not None
     ):
@@ -63,6 +72,5 @@ def comment_to_type(comment: str) -> Type:
             if m is not None
         }
         return Annotated[type_, msgspec.Meta(**annotation_metadata)] | None
-    # Based on some examples I found on Veeva's GitHub, attribute values seem to be
-    # nullable. See `tests/mdl_examples/scrapped` for plenty of examples
+
     return Any | None
