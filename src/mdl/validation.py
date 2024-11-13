26a8d616f8bc5d5240c3e1a3f11dbae8ad7c61dc
Gorka Eraña <gera@baselifescience.com>
2024-11-13 14:26:07 +0100

    Refactor 'comment_to_type'

--- src/mdl/validation.py
+++ src/mdl/validation.py
@@ -11,6 +11,34 @@ component_type_metadata = json.loads(
 )
 
 
+TYPE_PATTERN = re.compile(r"Type : (.*)")
+ENUM_PATTERN = re.compile(r"Allowed values : (.*)", flags=re.MULTILINE)
+MULTI_VALUE_PATTERN = re.compile(r"Allows multiple values")
+MAX_LEN_PATTERN = re.compile(r"Maximum length : (.*)")
+MIN_VAL_PATTERN = re.compile(r"Minimum value : (.*)")
+MAX_VAL_PATTERN = re.compile(r"Maximum value : (.*)")
+
+
+def is_enum(s: str) -> bool:
+    return ENUM_PATTERN.search(s) is not None
+
+
+def is_multi_value(s: str) -> bool:
+    return MULTI_VALUE_PATTERN.search(s) is not None
+
+
+def is_component_reference(matched_type_name: str) -> bool:
+    return (
+        # Is it a component type?
+        (matched_type_name in component_type_metadata.keys())
+        # Is it a subcomponent type?
+        or any(
+            matched_type_name in v["subcomponents"].keys()
+            for v in component_type_metadata.values()
+        )
+    )
+
+
 def comment_to_type(comment: str) -> Type:
     supported_types = {
         "String": str,
@@ -19,58 +47,62 @@ def comment_to_type(comment: str) -> Type:
         "LongString": str,
         "Enum": Literal,
         "XMLString": str,  # TODO: properly support
-        # "Expression": ET.Element,  # TODO: properly support
     }
-    type_match = re.search(r"Type : (.*)", comment)
+    type_match = TYPE_PATTERN.search(comment)
+    assert type_match is not None, "Type info should always be provided"
     matched_type_name = type_match.groups(0)[0]
-    max_len_match = re.search(r"Maximum length : (.*)", comment)
-    min_val_match = re.search(r"Minimum value : (.*)", comment)
-    max_val_match = re.search(r"Maximum value : (.*)", comment)
-    allowed_vals_match = re.search(r"Allowed values : (.*)", comment, re.MULTILINE)
-    allow_multiple_vals_match = re.search(r"Allows multiple values", comment)
-    type_ = supported_types.get(matched_type_name)
-    # Notice two things in what follows:
+    enum_match = ENUM_PATTERN.search(comment)
+    allowed_values = (
+        []
+        if enum_match is None
+        else [s for s in enum_match.groups(0)[0].split("|") if s]
+    )
+    # I believe two things are worth pointing out:
     # 1. All attribute values seem to be nullable, hence the `None` at every return.
     # PLenty of examples under `tests/mdl_examples/scrapped`.
     # 2. If an attribute value "allows multiple values", and it contains one item;
     # the parser will parse it as a single value, hence the `list[a_type] | a_type`
     # pattern below.
-
-    # `matched_type_name` might not map to any Python built-in type, but it might
-    # be a component type name
-    if type_ is None and (
-        # Is it a component type?
-        (matched_type_name in component_type_metadata.keys())
-        # Is it a subcomponent type?
-        or any(
-            matched_type_name in v["subcomponents"].keys()
-            for v in component_type_metadata.values()
-        )
+    match (
+        supported_types.get(matched_type_name),
+        is_component_reference(matched_type_name),
+        is_enum(comment),
+        is_multi_value(comment),
+        (
+            ("max_length", MAX_LEN_PATTERN.search(comment)),
+            ("ge", MIN_VAL_PATTERN.search(comment)),
+            ("le", MAX_VAL_PATTERN.search(comment)),
+        ),
     ):
-        # TODO: this current validation implementation does not allow to validate a list
-        # of references, as Annotated[list[str], msgspec.Meta(pattern=rf"^{matched_type_name}\.")]
-        # is not a valid msgspec type constraing
-        return Annotated[str, msgspec.Meta(pattern=rf"^{matched_type_name}\.")] | None
-    if (allow_multiple_vals_match is not None) and (allowed_vals_match is not None):
-        allowed_values = [s for s in allowed_vals_match.groups(0)[0].split("|") if s]
-        return list[Literal[*allowed_values]] | Literal[*allowed_values] | None
-    elif allow_multiple_vals_match is not None:
-        return list[type_] | type_ | None
-    elif allowed_vals_match is not None:
-        allowed_values = [s for s in allowed_vals_match.groups(0)[0].split("|") if s]
-        return Literal[*allowed_values] | None
-    if any(m is not None for m in [max_len_match, min_val_match, max_val_match]) and (
-        type_ is not None
-    ):
-        annotation_metadata = {
-            k: int(m.groups(0)[0])
-            for k, m in [
-                ("max_length", max_len_match),
-                ("ge", min_val_match),
-                ("le", max_val_match),
-            ]
-            if m is not None
-        }
-        return Annotated[type_, msgspec.Meta(**annotation_metadata)] | None
-
-    return Any | None
+        # Reference to other components
+        case (None, True, _, _, _):
+            # TODO: this current validation implementation does not allow to validate a list
+            # of references, as Annotated[list[str], msgspec.Meta(pattern=rf"^{matched_type_name}\.")]
+            # is not a valid msgspec type constraint
+            return (
+                Annotated[str, msgspec.Meta(pattern=rf"^{matched_type_name}\.")] | None
+            )
+        # Multi-value enum
+        case (_, _, True, True, _):
+            return list[Literal[*allowed_values]] | Literal[*allowed_values] | None
+        # Multi-value
+        case (type_, _, False, True, _):
+            return list[type_] | type_ | None
+        # Enum
+        case (_, _, True, False, _):
+            return Literal[*allowed_values] | None
+        # Constraints
+        case (type_, _, _, _, constraints) if (type_ is not None) and any(
+            m is not None for _, m in constraints
+        ):
+            annotation_metadata = {
+                k: int(m.groups(0)[0]) for k, m in constraints if m is not None
+            }
+            return Annotated[type_, msgspec.Meta(**annotation_metadata)] | None
+        case (type_, _, _, _, _) if type_ is not None:
+            return type_ | None
+        # Fallback
+        case _:
+            print(type_, comment)
+            # TODO: perhaps handle the rest differently?
+            return Any | None
